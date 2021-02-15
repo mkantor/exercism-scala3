100c653d990a07e8deb2e2d985ad21631044e828
Matt Kantor <the.matt.kantor@gmail.com>
2021-02-15 11:44:37 -0800

    Implement space age.

--- space-age/src/test/scala/SpaceAgeTest.scala
+++ space-age/src/test/scala/SpaceAgeTest.scala
@@ -9,37 +9,30 @@ class LeapTest extends AnyFunSpec with Matchers {
   }
 
   it("age on Mercury") {
-    pending
     SpaceAge.onMercury(2134835688) should be (280.88 +- 0.01)
   }
 
   it("age on Venus") {
-    pending
     SpaceAge.onVenus(189839836) should be (9.78 +- 0.01)
   }
 
   it("age on Mars") {
-    pending
     SpaceAge.onMars(2.329871239E9) should be (39.25 +- 0.01)
   }
 
   it("age on Jupiter") {
-    pending
     SpaceAge.onJupiter(901876382) should be (2.41 +- 0.01)
   }
 
   it("age on Saturn") {
-    pending
     SpaceAge.onSaturn(3.0E9) should be (3.23 +- 0.01)
   }
 
   it("age on Uranus") {
-    pending
     SpaceAge.onUranus(3.210123456E9) should be (1.21 +- 0.01)
   }
 
   it("age on Neptune") {
-    pending
     SpaceAge.onNeptune(8.210123456E9) should be (1.58 +- 0.01)
   }
 }
