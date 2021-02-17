7744f982940e60a1ec22e74cfb28e0696fef8ea6
Matt Kantor <the.matt.kantor@gmail.com>
2021-02-16 20:59:12 -0800

    Implement word count.

--- word-count/src/test/scala/WordCountTest.scala
+++ word-count/src/test/scala/WordCountTest.scala
@@ -9,55 +9,46 @@ class WordCountTest extends AnyFunSpec with Matchers {
   }
 
   it("count one of each word") {
-    pending
     WordCount("one of each").countWords should be(
       Map(("one", 1), ("of", 1), ("each", 1)))
   }
 
   it("multiple occurrences of a word") {
-    pending
     WordCount("one fish two fish red fish blue fish").countWords should be(
       Map(("blue", 1), ("two", 1), ("fish", 4), ("one", 1), ("red", 1)))
   }
 
   it("handles cramped lists") {
-    pending
     WordCount("one,two,three").countWords should be(
       Map(("one", 1), ("two", 1), ("three", 1)))
   }
 
   it("handles expanded lists") {
-    pending
     WordCount("one,\ntwo,\nthree").countWords should be(
       Map(("one", 1), ("two", 1), ("three", 1)))
   }
 
   it("ignore punctuation") {
-    pending
     WordCount("car: carpet as java: javascript!!&@$%^&").countWords should be(
       Map(("as", 1), ("car", 1), ("java", 1), ("carpet", 1), ("javascript", 1)))
   }
 
   it("include numbers") {
-    pending
     WordCount("testing, 1, 2 testing").countWords should be(
       Map(("testing", 2), ("1", 1), ("2", 1)))
   }
 
   it("normalize case") {
-    pending
     WordCount("go Go GO Stop stop").countWords should be(
       Map(("go", 3), ("stop", 2)))
   }
 
   it("with apostrophes") {
-    pending
     WordCount("First: don\'t laugh. Then: don\'t cry.").countWords should be(
       Map(("laugh", 1), ("don't", 2), ("then", 1), ("first", 1), ("cry", 1)))
   }
 
   it("with quotations") {
-    pending
     WordCount("Joe can\'t tell between \'large\' and large.").countWords should be(
       Map(("can't", 1),
           ("large", 2),
@@ -68,7 +59,6 @@ class WordCountTest extends AnyFunSpec with Matchers {
   }
 
   it("multiple spaces not detected as a word") {
-    pending
     WordCount(" multiple   whitespaces").countWords should be(
       Map(("multiple", 1), ("whitespaces", 1)))
   }
